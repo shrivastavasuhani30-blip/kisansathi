d6183e6ea84110be5ad96c2888a59f5b67fa1f60
shrivastavasuhani30-blip <shrivastavasuhani30@gmail.com>
2026-05-27 13:48:30 +0530

    fix: layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,33 @@
-ÿþ
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "KisanSathi",
+  description: "MP/UP ke farmers ke liye bank aur loan guide",
+  verification: {
+    google: "O3jigwToEM3b49Z_PcO7pNq7c0gdBEnXN9ca2Nk9Qp8",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
