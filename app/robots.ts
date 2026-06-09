9e3545863349b835247550f33e1b09277c447f95
shrivastavasuhani30-blip <shrivastavasuhani30@gmail.com>
2026-06-09 20:35:25 +0530

    add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://kisansathi-rjmv.vercel.app/sitemap.xml',
+  }
+}
